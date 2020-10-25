8a5dbf32ba8ab94978a5da6ab7eb0156a72f8e27
Boutissante Issam <boutissante.issam.dev@gmail.com>
2020-10-25 22:07:42 +0100

    add delegates and Geniric

---
+++ Genirics/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Genirics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            showTypeOf <string> showTypeOf = new showTypeOf<string>();
+            Type type = showTypeOf.getType();
+            Console.WriteLine(type);
+        }
+    }
+    internal class showTypeOf<T> where T : class
+    {
+        public Type getType()
+        {
+            return typeof(T);
+        }
+    }
+
+    internal class constraintClass
+    {
+        public T add<T>(T a, T b) where T : IComparable
+        {
+            return a.CompareTo(b) > 0 ? a : b;
+        }
+    }
+}
---
+++ delegates/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace delegates
+{
+    class Program
+    {
+        delegate int operation(int a, int b);
+        static void Main(string[] args)
+        {
+
+            int result =multiply()(2, 4);
+            Console.WriteLine(result);
+
+
+        }
+
+        static operation multiply()
+        {
+            return (int a, int b) =>
+            {
+                return a * b;
+            };
+        }
+
+
+        static void calculate(operation op)
+        {
+            Console.WriteLine(op(2,3));
+        }
+        static int addition(int a,int b)
+        {
+            return a + b;
+        }
+
+
+    }
+}
