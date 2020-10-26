083fddf57340f72ec9e8cea5a9db03e3fb108d24
Boutissante Issam <boutissante.issam.dev@gmail.com>
2020-10-26 20:03:59 +0100

    adding Extention Methods

---
+++ Extension_Methods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator(1,1);
+            calculator.ShowAddition();
+            calculator.ShowMultiplication();
+
+            Console.ReadKey();
+        }
+    }
+    static class Extensions
+    {
+        internal static void ShowMultiplication(this Calculator cal)
+        {
+            Console.WriteLine("the result of multiplication is : " + (cal.Number_1 + cal.Number_2));
+        }
+    }
+}
